050f46770831c595097ae05ca08f5eb400ff3683
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-07 15:11:15 +0000

    Adjust standing wave control panel height

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-07T10:47:32Z';
+const BUILD_ID = '2026-02-07T11:06:27Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
