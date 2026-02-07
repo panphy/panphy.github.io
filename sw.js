6c027aac9567b2e459b1b89a5d2f3ef561de37a7
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-07 10:39:09 +0000

    Disable selection in markdown presentation mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-07T09:21:46Z';
+const BUILD_ID = '2026-02-07T09:36:12Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
