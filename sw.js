6a2da22a4874dc096b35191dc287cc8f92976397
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-07 15:44:37 +0000

    Update motion tracker theme toggle icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-07T12:47:00Z';
+const BUILD_ID = '2026-02-07T12:56:42Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
