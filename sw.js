8479741bc91920c341eb0299317a83b75d623120
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-10 22:33:51 +0000

    Change PanPhyPlot image export to PNG at 2x scale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-10T22:23:51Z';
+const BUILD_ID = '2026-02-10T22:32:30Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
