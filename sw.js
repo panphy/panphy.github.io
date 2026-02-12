07430bb1804018439c49ae2ce4245a1b4ca7a020
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-12 15:20:51 +0000

    Update PanPhyPlot view table save button label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-13T00:10:00Z';
+const BUILD_ID = '2026-02-12T15:17:33Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
