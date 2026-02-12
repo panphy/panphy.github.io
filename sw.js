b87b4d05186f62d1527b9828ea65c257c88265f3
PanPhy <66046319+panphy@users.noreply.github.com>
2026-02-12 22:22:01 +0000

    Update BUILD_ID in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-12T23:32:00Z';
+const BUILD_ID = '2026-02-12T23:35:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
