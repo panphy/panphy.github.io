becbebc32f051006554886e5581e944d9de7b220
PanPhy <ypleung@gmail.com>
2026-02-18 11:53:53 +0000

    minor UI update for panphyplot

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-18T18:45:00Z';
+const BUILD_ID = '2026-02-18T18:55:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
