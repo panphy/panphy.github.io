e8adc83c2ed7df04a931ef14a31b0a80b5fe8eaf
PanPhy <ypleung@gmail.com>
2026-02-20 17:55:49 +0000

    update offline ready indicator in index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-20T23:40:00Z';
+const BUILD_ID = '2026-02-20T23:52:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
