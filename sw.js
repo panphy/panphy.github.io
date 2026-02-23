f83876e298693772181f0683ea7fe867e4f1676c
PanPhy <ypleung@gmail.com>
2026-02-23 19:54:14 +0000

    update index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-23T22:46:00Z';
+const BUILD_ID = '2026-02-23T22:48:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
