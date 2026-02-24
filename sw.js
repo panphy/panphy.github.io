087fdf89aefdaaaf5af41d8cf35406b34488ed57
PanPhy <ypleung@gmail.com>
2026-02-24 17:40:00 +0000

    UI fix: tone_generator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-24T17:29:50Z';
+const BUILD_ID = '2026-02-24T17:37:59Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
