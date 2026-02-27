dbbc8d983fb9ddd4156f6e1a0e8ff63cceed59bc
PanPhy <ypleung@gmail.com>
2026-02-27 10:23:21 +0000

    minor UI update: lorentz

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-02-27T22:00:00Z';
+const BUILD_ID = '2026-02-27T22:15:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
