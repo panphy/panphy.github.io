9e99814a19d2c544960a6b315e1037e42fdc69e1
PanPhy <ypleung@gmail.com>
2026-05-10 13:17:00 +0100

    Split ripple tank modes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-05-10T11:49:57Z';
+const BUILD_ID = '2026-05-10T12:15:40Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
