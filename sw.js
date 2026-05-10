405f22abc7c3fb8fd96d83b1fe5e0be60dc048c3
PanPhy <ypleung@gmail.com>
2026-05-10 13:57:31 +0100

    Bump BUILD_ID after index.html card order change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-05-10T12:36:56Z';
+const BUILD_ID = '2026-05-10T13:00:00Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
