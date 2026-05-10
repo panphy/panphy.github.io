eac4cca5d53ad555bb47886e9b77f6715154f854
PanPhy <ypleung@gmail.com>
2026-05-10 13:33:14 +0100

    Redesign ripple tank mode selection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = '2026-05-10T12:25:14Z';
+const BUILD_ID = '2026-05-10T12:31:55Z';
 const CACHE_PREFIX = 'panphy-labs';
 const PRECACHE_NAME = `${CACHE_PREFIX}-precache-${BUILD_ID}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}-runtime-${BUILD_ID}`;
